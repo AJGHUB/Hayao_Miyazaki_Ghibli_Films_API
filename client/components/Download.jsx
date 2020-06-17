00b59bca1248a0fcfe1884a21fe39fc786b8befd
Boyd_Thurston <boydthurston@gmail.com>
2020-06-18 11:54:38 +1200

    Download list

---
+++ client/components/Download.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+
+class Download extends React.Component {
+  state = {
+    images: [
+      {name: "" ,
+      url: ""},
+      {name: "arietty1" ,
+      url: "/images/arietty1.jpg"},
+      {name: "chihiro1" ,
+      url: "/images/chihiro1.jpg"},
+      {name: "cittàIncantata" ,
+      url: "/images/cittàIncantata.jpg"},
+      {name: "ghilbli" ,
+      url: "/images/ghibli.jpg"},
+      {name: "howl1" ,
+      url: "/images/howl1.jpg"},
+      {name: "kaguya1" ,
+      url: "/images/kaguya1.jpg"},
+      {name: "laputa1" ,
+      url: "/images/laputa1.jpg"},
+    ]
+  }
+
+  render(){
+    return (
+      <>
+        
+      </>
+    )
+  }
+}
+
+export default Download

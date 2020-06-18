987b48826c502ee431f325bb54a30ad9f491ca17
Alice Jowsey <12345alcatraz@gmail.com>
2020-06-18 13:31:52 +1200

    landing boxes in place

--- client/components/Landing.jsx
+++ client/components/Landing.jsx
@@ -10,11 +10,11 @@ render() {
 
     return (
         <>
-          <div className='landing-col'>
+          <div className='landing-col-1'>
             <Link to='/movie'>Ghibli Movies</Link>
           </div>
-          <div className='landing-col'>.</div>
-          <div className='landing-col'>.</div>
+          <div className='landing-col-2'>.</div>
+          <div className='landing-col-3'>.</div>
         </>
     )
   }

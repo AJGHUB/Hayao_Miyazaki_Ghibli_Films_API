e5943e7c5335f9a7a941484bcd58ace0db685d81
Boyd_Thurston <boydthurston@gmail.com>
2020-06-26 11:59:53 +1200

    polished app layout

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -18,16 +18,16 @@ class App extends React.Component {
     return ( 
      <Router>
       <header >
-      <h1> Hayao Miyazaki </h1> <img src = "/Studio_Ghibli_logo.svg"/>
+        <h1> Hayao Miyazaki </h1> <img src = "/Studio_Ghibli_logo.svg"/>
       </header>
       <div >
-      <main className = "flex-grid" >
-      <Route exact path = "/" component = { Landing} />
-      <Route path = "/movie" component = { Movie } /> 
-      <Route path = "/download" component = { Download }/> 
-      </main>  
+        <main className = "flex-grid" >
+          <Route exact path = "/" component = { Landing} />
+          <Route path = "/movie" component = { Movie } /> 
+          <Route path = "/download" component = { Download }/> 
+        </main>  
       </div> 
-      </Router>
+    </Router>
     )
   }
 }

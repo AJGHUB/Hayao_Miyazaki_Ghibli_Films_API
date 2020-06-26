325bf8a8473ee500782a5ec1d659cddde3289acd
Alice Jowsey <12345alcatraz@gmail.com>
2020-06-26 23:46:07 +1200

    css design

--- client/components/Landing.jsx
+++ client/components/Landing.jsx
@@ -8,17 +8,44 @@ export const Landing = () => {
       <main className='flex-grid'>
         <div className='landing-col-1'>
           <h3>Ghibli Fan Links</h3>
-          <p><a target="_blank" href="http://www.ghibli.jp/">ghibli.jp</a></p> 
-          <p><a target="_blank" href="http://www.ghibli-museum.jp/en/">ghibli museum</a></p>
-          <p><a target="_blank" href="https://ghibli.fandom.com/wiki/Ghibli_Wiki">Ghibli wiki</a></p>
-          <p><a target="_blank" href="https://gkids.com/films/">ghibli films</a></p>
+          <p>
+            <Link to='http://www.ghibli.jp/' target='_blank'>
+              The Ghibli Studio 
+            <img className='ghibli-totoro' src='/images/ghibli.png'/>
+            </Link>
+          </p>
+          <p>
+            <Link to='http://www.openculture.com/2020/05/a-virtual-tour-inside-the-hayao-miyazakis-studio-ghibli-museum.html' target='_blank'>
+              Ghibli Virtual Tour Museum
+            </Link>
+          </p>
+          <p>
+            <Link to='https://ghibli.fandom.com/wiki/Ghibli_Wiki'
+              target='_blank'>
+              Ghibli wiki
+            </Link>
+          </p>
+          <p>
+            <Link to='_blank' href='https://gkids.com/films/'>
+              ghibli films
+            </Link>
+          </p>
         </div>
         <div className='landing-col-2'>
-          <Link to='/download'>Ghibli Wallpapers to Download</Link>
+          <h3 className='heading1'>Wallpapers</h3>
+          <Link to='/download'>
+            <img
+              className='gif-movie'
+              src='https://media3.giphy.com/media/CRJqX2pz2RA8E/giphy.gif'
+              alt='ghibli door'
+            />
+          </Link>
         </div>
         <div className='landing-col-3'>
-          <Link to='/movie'>Click to see Ghibli Movies</Link>
-          <Link to='/movie'><img src="https://media3.giphy.com/media/CRJqX2pz2RA8E/giphy.gif" width="100%" height="100%" alt="ghibli door"/></Link>
+          <h3 className='heading2'>Movies</h3>
+          <Link to='/movie'>
+          <iframe className='ghibli-movie' width="560" height="315" src="https://www.youtube.com/embed/8HGjm0Sk8tk" frameBorder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+          </Link>
         </div>
       </main>
     </>
